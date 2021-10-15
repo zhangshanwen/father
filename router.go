1a983a9513f6403ec80e413790503c8c1e51603e
张善文 <763625787@qq.com>
2021-10-15 15:00:39 +0800

    fix:     1.删除无用代码

--- router.go
+++ router.go
@@ -1,39 +1,9 @@
 package father
 
-import (
-	"net/http"
-)
-
 type (
 	Router struct {
 		Method   string
 		Path     string
-		Handler  HandlerFunc
 		Handlers []HandlerFunc
 	}
 )
-
-func (f *Father) addRouter(method, path string, handler HandlerFunc) {
-	f.Routers = append(f.Routers, Router{
-		Method:  method,
-		Path:    path,
-		Handler: handler,
-	})
-}
-
-func (f *Father) Post(path string, handler HandlerFunc) {
-	f.addRouter(http.MethodPost, path, handler)
-}
-func (f *Father) Get(path string, handler HandlerFunc) {
-	f.addRouter(http.MethodGet, path, handler)
-}
-
-func (f *Father) Put(path string, handler HandlerFunc) {
-	f.addRouter(http.MethodPut, path, handler)
-}
-func (f *Father) Delete(path string, handler HandlerFunc) {
-	f.addRouter(http.MethodDelete, path, handler)
-}
-func (f *Father) Patch(path string, handler HandlerFunc) {
-	f.addRouter(http.MethodPatch, path, handler)
-}
